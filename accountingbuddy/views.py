ef8f995bbddd2b4904d8b71b67525e925fe23f7b
pythonart <31215688+pythonart@users.noreply.github.com>
2017-09-08 09:34:42 +0530

    Update views.py

--- accountingbuddy/views.py
+++ accountingbuddy/views.py
@@ -16,8 +16,9 @@ from django.contrib.auth.decorators import login_required, permission_required
 from django.urls import reverse_lazy
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin
-from accountingbuddy.models import MyProfile, Pricing, Business_request
 
+from accountingbuddy.models import MyProfile, Pricing, Business_request
+from .forms import BusinessRequestForm, 
 
 # Create your views here.
 
