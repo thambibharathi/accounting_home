f29f131b87be7e1b6c532b0ddf98143a3290b2b1
pythonart <31215688+pythonart@users.noreply.github.com>
2017-09-09 02:06:17 +0530

    Update forms.py

--- accountingbuddy/forms.py
+++ accountingbuddy/forms.py
@@ -10,9 +10,9 @@ class BusinessRequestForm(forms.Form):
 	license_type =forms.ModelChoiceField(label='Select the license type',queryset=Pricing.objects.none(),help_text="Check Pricing on Main Page Pricing")
 	additional_detail=forms.CharField(label='Enter any additional details', widget=forms.Textarea, help_text="Give details about your Tax Structure", required=False)
 	
-	def __init__(self,user,*args,**kwargs):
+	def __init__(self,input_user,*args,**kwargs):
 		super(BusinessRequestForm,self).__init__(*args,**kwargs)
-		select_user=MyProfile.objects.get(user=user)
+		select_user=MyProfile.objects.get(user=input_user)
 		price=Pricing.objects.all().filter(pricing_region=select_user.region).filter(target=select_user.sales_partner)
 		self.fields['license_type'].queryset=price
 		
