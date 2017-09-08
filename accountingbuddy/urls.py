fe183078446695c0aa102e1712a7f3efa185533f
pythonart <31215688+pythonart@users.noreply.github.com>
2017-09-09 02:40:23 +0530

    Update urls.py

--- accountingbuddy/urls.py
+++ accountingbuddy/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import url, include
 from . import views
 from django.conf import settings
 from django.views.static import serve
+from django.views.generic import TemplateView
 
 
 app_name='accountingbuddy'
 urlpatterns=[  
 url(r'^pricing/india/$',views.pricing_india,name='pricing-india'),
 url(r'^businessrequest/submit/$',views.businessRequestFormView,name='business-req'),
+url(r'^thankyou/$',TemplateView.as_view(template_name='thanks.html'),name='thanks'),  
 ]
