914cefe95f4ea2fba66fd7ef4b69a9e8b202849b
pythonart <31215688+pythonart@users.noreply.github.com>
2017-09-08 09:28:31 +0530

    Update urls.py

--- accountingbuddy/urls.py
+++ accountingbuddy/urls.py
@@ -7,4 +7,5 @@ from django.views.static import serve
 app_name='accountingbuddy'
 urlpatterns=[  
 url(r'^pricing/india/$',views.pricing_india,name='pricing-india'),
+url(r'^businessrequest/submit/$',views.businessFormCreateView.as_view(),name='business-req'),
 ]
