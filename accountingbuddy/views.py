e1c73259a23ffed0c46cd4b39fbee97337805739
pythonart <31215688+pythonart@users.noreply.github.com>
2017-09-09 03:10:17 +0530

    Update views.py

--- accountingbuddy/views.py
+++ accountingbuddy/views.py
@@ -56,7 +56,8 @@ def businessRequestFormView(request):
 			sender='info@accountingbuddy.org'
 			subject="AccountingBuddy.Org Business Setup Request Fm %s" % user.first_name
 			message="Business Name : %s , Business Type: %s , License Type: %s, Additional Details : %s , User %s , Phone %s, Email %s" % (business_name,business_type,license_type, additional_details, request.user,user.myprofile.phone_no,user.email)
-			recipients = ['keeganpatrao@gmail.com']
+			recipients = ['keeganpatrao@gmail.com',]
+			recipients +=[user.email,]
 			send_mail(subject, message, sender, recipients)
 			return HttpResponseRedirect(reverse('accountingbuddy:thanks'))
 	else:
