470adb6c5d123c5b5a27e6b50e3239b9cf0cf974
pythonart <31215688+pythonart@users.noreply.github.com>
2017-09-08 12:42:23 +0530

    Update forms.py

--- accountingbuddy/forms.py
+++ accountingbuddy/forms.py
@@ -6,4 +6,9 @@ class BusinessRequestForm(forms.ModelForm):
   class Meta:
     model=Business_request
     fields=['business_name','business_type','license_type','additional_details', ]  
-
+    
+  def __init__(self, *args, **kwargs):
+        super(BusinessRequestForm, self).__init__(*args, **kwargs)
+	    	user=MyProfile.objects.get(user=request.user) 
+		    price=Pricing.objects.all().filter(pricing_region=user.region).filter(target=user.sales_partner)
+        self.license_type=forms.ModelChoiceField(queryset=price) 
