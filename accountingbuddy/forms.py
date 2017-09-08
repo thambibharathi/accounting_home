14e2d39aa3fa75354c2d0af2bc4f1c65bcbdcb23
pythonart <31215688+pythonart@users.noreply.github.com>
2017-09-08 08:37:14 +0530

    Update forms.py

--- accountingbuddy/forms.py
+++ accountingbuddy/forms.py
@@ -1,4 +1,9 @@
 from django import forms
 
-from .models import  *
+from .models import  Business_request
+
+class BusinessRequestForm(forms.ModelForm):
+  class Meta:
+    model=Business_request
+    fields=['business_name','business_type','license_type','additional_details', ]  
 
