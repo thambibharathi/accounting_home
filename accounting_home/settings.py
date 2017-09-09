52c4e11cf0a2044f9abec42d4b2546584f5bac11
pythonart <31215688+pythonart@users.noreply.github.com>
2017-09-09 11:42:22 +0530

    Update settings.py

--- accounting_home/settings.py
+++ accounting_home/settings.py
@@ -362,15 +362,4 @@ EMAIL_HOST_USER='info@accountingbuddy.org'
 #EMAIL_HOST_PASSWORD='password'  Refer local settings
 EMAIL_USE_TLS=True
 
-#File Upload Settings
-# Add to your settings file
-CONTENT_TYPES = ['image', 'video']
-# 2.5MB - 2621440
-# 5MB - 5242880
-# 10MB - 10485760
-# 20MB - 20971520
-# 50MB - 5242880
-# 100MB 104857600
-# 250MB - 214958080
-# 500MB - 429916160
-MAX_UPLOAD_SIZE = 214958080
+
