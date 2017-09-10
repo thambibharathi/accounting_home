f13d3db3f28ae6fb5d91a431f9dee7c6961e8a33
pythonart <31215688+pythonart@users.noreply.github.com>
2017-09-11 03:40:02 +0530

    Update admin.py

--- accountingbuddy/admin.py
+++ accountingbuddy/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Pricing,  Business_request
+from .models import Pricing,  Business_request, SendMails
 
 # Register your models here.
 
 admin.site.register(Pricing)
 admin.site.register(Business_request)
+admin.site.register(SendMails)
+
 
