cf8026c880b765a9aed41e07c1caf5a495ba27df
pythonart <31215688+pythonart@users.noreply.github.com>
2017-09-10 12:55:48 +0530

    Update views.py

--- accountingbuddy/views.py
+++ accountingbuddy/views.py
@@ -103,7 +103,7 @@ def businessRequestFormView(request):
 			send_mail(subject, message, sender, recipients)
 			return HttpResponseRedirect(reverse('accountingbuddy:thanks'))
 	else:
-		form = BusinessRequestForm(instance=s)
+		form = BusinessRequestForm()
 	return render(request, 'business_request_form.html', {'form': form})
 
 
