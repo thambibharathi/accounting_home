22f19387442f09b902e7fcfb17e9bfec209d591a
pythonart <31215688+pythonart@users.noreply.github.com>
2017-09-30 23:12:03 +0530

    Update models.py

--- business/models.py
+++ business/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Business(models.Model):
   name=models.CharField("Business Name",max_length=50)
   code=models.CharField("Code",max_length=200)
-  user=models.OneToOneField("auth.User")
+  user=models.ForeignKey("auth.User")
   
   def __str__(self):
     return "%s" % self.name
