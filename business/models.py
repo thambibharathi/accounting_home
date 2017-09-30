47c4a49a72884c16006e45722531b0944b177cdc
pythonart <31215688+pythonart@users.noreply.github.com>
2017-09-30 23:06:14 +0530

    Update models.py

--- business/models.py
+++ business/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Business(models.Model):
-  name=models.CharField("Business Name",max_length="50")
-  code=models.CharField("Code",max_length="200")
+  name=models.CharField("Business Name",max_length=50)
+  code=models.CharField("Code",max_length=200)
   user=models.OneToOneField("auth.User")
   
   def __str__(self):
