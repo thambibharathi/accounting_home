6465275e49bed3108fcf080f2a06736a163c99f4
pythonart <31215688+pythonart@users.noreply.github.com>
2017-10-02 05:37:41 +0530

    Update models.py

--- business/models.py
+++ business/models.py
@@ -6,8 +6,11 @@ class Business(models.Model):
   name=models.CharField("Business Name",max_length=50)
   code=models.CharField("Code",max_length=200)
   user=models.ForeignKey("auth.User")
-  
+  created = models.DateTimeField(auto_now_add=True)
+
   def __str__(self):
     return "%s" % self.name
+  
+  
   
   
