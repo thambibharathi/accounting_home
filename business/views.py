dac306b25f84f3edf59d3c6bc26e6f22a0e90e08
pythonart <31215688+pythonart@users.noreply.github.com>
2017-10-02 05:46:36 +0530

    Update views.py

--- business/views.py
+++ business/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.views.generic.edit import UpdateView , CreateView, DeleteView
+from django.shortcuts import render, render_to_response
 
-# Create your views here.
+from django.shortcuts import get_object_or_404 , get_list_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+from django.views.generic.edit import UpdateView , CreateView, DeleteView
+from django.views.generic.detail import DetailView
+from django.utils import timezone
+from django.template import loader
+from django.http import HttpResponse
+from django.http import Http404
+from django.contrib.auth.decorators import login_required, permission_required
+from django.urls import reverse_lazy
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin
+from django.core.mail import send_mail , BadHeaderError
+from django.core.files.storage import FileSystemStorage
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.core.mail import EmailMultiAlternatives
+
+from business.models import Business
+
+class BusinessCreate(CreateView):
+  model=Business
+  
+  
