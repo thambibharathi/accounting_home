b37e05d154d39ac0e6b5424919b8b8c82511fa82
pythonart <31215688+pythonart@users.noreply.github.com>
2017-10-02 05:58:22 +0530

    Update models.py

--- business/models.py
+++ business/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Business(models.Model):
   name=models.CharField("Business Name",max_length=50)
-  code=models.CharField("Code",max_length=200)
+  code=models.CharField("Code",max_length=200,null=True,blank=True)
   user=models.ForeignKey("auth.User")
   created = models.DateTimeField(auto_now_add=True)
 
