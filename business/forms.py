2e1291b0d52ed74059f837f3ea450e1bdb004c7c
pythonart <31215688+pythonart@users.noreply.github.com>
2017-10-03 09:13:47 +0530

    Update forms.py

--- business/forms.py
+++ business/forms.py
@@ -8,19 +8,12 @@ class BusinessCreateForm(ModelForm):
   class Meta:
     model=Business
     fields=['name']
-  def save(self,user):
-    bus=super(BusinessCreateForm,self).save(commit=False)
-    bus.user=user
-    bus.code='code'
-    bus.save()
-    return bus
-    
-    '''
+ 
     def __init__(self,*args,**kwargs):
       self.request=kwargs.pop('request',None)
       super(BusinessCreateForm,self).__init__(*args,**kwargs)
       self.fields['user']=request.user
       self.fields['code']='code'
-      '''
+  
       
 
