d2e60119bc9c097dc53155f5dc762238a9e29fc5
pythonart <31215688+pythonart@users.noreply.github.com>
2017-10-03 14:58:16 +0530

    Update forms.py

--- business/forms.py
+++ business/forms.py
@@ -8,11 +8,7 @@ class BusinessCreateForm(ModelForm):
   class Meta:
     model=Business
     fields=['name']
-  def __init__(self,*args,**kwargs):
-      self.request=kwargs.pop('request',None)
-      super(BusinessCreateForm,self).__init__(*args,**kwargs)
-      self.fields['user']=request.user
-      self.fields['code']='code'
+
   
       
 
