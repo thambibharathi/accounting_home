6ef0b7b32d998ad1230e054011dc291a6cad6c0d
pythonart <31215688+pythonart@users.noreply.github.com>
2017-10-03 08:45:58 +0530

    Update forms.py

--- business/forms.py
+++ business/forms.py
@@ -5,4 +5,21 @@ from django.core.exceptions import ValidationError
 from .models import  Business
 
 Class BusinessCreateForm(ModelForm):
-  
+  class Meta:
+    model=Business
+    fields=['name']
+
+def save(self,user):
+  bus=super(BusinessCreateForm,self).save(commit=False)
+  bus.user=user
+  bus.code='code'
+  bus.save()
+  return bus
+
+'''    
+    def __init__(self,*args,**kwargs):
+      self.request=kwargs.pop('request',None)
+      super(BusinessCreateForm,self).__init__(*args,**kwargs)
+      self.fields['user']=request.user
+      
+  '''
