ee6f39647b62e6d18329f1047053f6bc1d7fe622
pythonart <31215688+pythonart@users.noreply.github.com>
2017-10-06 18:15:51 +0530

    Update forms.py

--- accountingbuddy/forms.py
+++ accountingbuddy/forms.py
@@ -28,9 +28,9 @@ class BusinessRequestForm(ModelForm):
 		
 		
 class MyCustomProfileForm(ProfileForm):
-    def save(self, *args, **kwargs):
-        user = super(MyCustomProfileForm, self).save(*args, **kwargs)
-	if self._signup:
-		passwd=self.cleaned_data.get('password1')
-		print(passwd)
-	return user
+    	def save(self, *args, **kwargs):
+		user = super(MyCustomProfileForm, self).save(*args, **kwargs)
+		if self._signup:
+			passwd=self.cleaned_data.get('password1')
+			print(passwd)
+		return user
