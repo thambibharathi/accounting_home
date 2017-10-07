1f73ab0b452a750a7acf92c4fe738c7ac0423a87
pythonart <31215688+pythonart@users.noreply.github.com>
2017-10-07 05:40:13 +0530

    Update urls.py

--- business/urls.py
+++ business/urls.py
@@ -3,10 +3,11 @@ from . import views
 from django.conf import settings
 from django.views.static import serve
 from django.views.generic import TemplateView
+from business.views import BusinessListView
 
 
 app_name='business'
 urlpatterns=[  
 url(r'^business/create/$',views.BusinessCreateView,name='business-create'),
-url(r'^business/list/$',views.BusinessListView.as_view(),name='business-list'),  
+url(r'^business/list/$',BusinessListView.as_view(),name='business-list'),  
 ]
