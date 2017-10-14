43183f17c25b9c5e8f704d01ebf03513c5a60e7e
pythonart <31215688+pythonart@users.noreply.github.com>
2017-10-14 10:32:36 +0530

    Update manager_models.py

--- business/manager_models.py
+++ business/manager_models.py
@@ -1,2 +1,12 @@
 class BusinessDetails:
-  
+  def __init__(self):
+    self.tradingName=tradingName
+    self.contactDetails=contactDetails
+    self.businessIdentifier=businessIdentifier
+    
+class CustomerDetails:
+  def __init__(self):
+    self.name=customerName
+    self.billingAddress=billingAddress
+    self.email=email
+    self.
