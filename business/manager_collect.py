03b40b61b679adbd19daedc2d828d98b440e2160
pythonart <31215688+pythonart@users.noreply.github.com>
2017-10-17 18:47:12 +0530

    Update manager_collect.py

--- business/manager_collect.py
+++ business/manager_collect.py
@@ -21,7 +21,7 @@ for tax in tcodes:
   taxli.append(TaxCode(tcodes[tax],tax))
   
              
-def get_tax_code(taxli=taxli,taxcode):
+def get_tax_code(taxcode,taxli=taxli):
   for item in taxli:
     if item.code == taxcode:
       return item
